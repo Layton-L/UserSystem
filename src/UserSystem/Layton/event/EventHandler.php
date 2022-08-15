51fc5736d25206c1101348fbd6f47b7343b222f5
Demky <nemoaleksey20054@gmail.com>
2022-08-15 14:29:35 +0300

    update

--- src/UserSystem/Layton/event/EventHandler.php
+++ src/UserSystem/Layton/event/EventHandler.php
@@ -4,10 +4,16 @@ declare(strict_types = 1);
 
 namespace UserSystem\Layton\event;
 
+use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerTransferEvent;
 use pocketmine\player\GameMode;
+use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
 use UserSystem\Layton\form\LoginForm;
 use UserSystem\Layton\form\RegistrationForm;
@@ -16,14 +22,38 @@ use UserSystem\Layton\UserSystem;
 
 class EventHandler implements Listener {
 
-    public function onPlayerChat(PlayerChatEvent $event): void {
-        $player = $event->getPlayer();
+    public function onEntityTeleport(EntityTeleportEvent $event): void {
+        if (($player = $event->getEntity()) instanceof Player) {
+            if (!UserSystem::isLogined($player)) {
+                $event->cancel();
+            }
+        }
+    }
 
-        if (!UserSystem::isLogined($player)) {
+    public function onPlayerChat(PlayerChatEvent $event): void {
+        if (!UserSystem::isLogined($event->getPlayer())) {
             $event->cancel();
         }
 
         $event->setMessage(PasswordUtils::checkString($event->getMessage()));
+    }
+
+    public function onPlayerDropItem(PlayerDropItemEvent $event): void {
+        if (!UserSystem::isLogined($event->getPlayer())) {
+            $event->cancel();
+        }
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
+        if (!UserSystem::isLogined($event->getPlayer())) {
+            $event->cancel();
+        }
+    }
+
+    public function onPlayerItemUse(PlayerItemUseEvent $event): void {
+        if (!UserSystem::isLogined($event->getPlayer())) {
+            $event->cancel();
+        }
     }
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
@@ -39,6 +69,8 @@ class EventHandler implements Listener {
                 $message = $queryHelper->getTranslatedString("module.login.timeout");
                 $player->kick($message, $message);
             }
+
+            LoginForm::setTries($player, 0);
         }), 20 * (int) UserSystem::getInstance()->getConfig()->get("entry_time"));
 
         if (!UserSystem::getInstance()->getDataManager()->isRegistered($player)) {
@@ -60,6 +92,12 @@ class EventHandler implements Listener {
 
         UserSystem::login($player);
         $player->sendMessage($queryHelper->getTranslatedString("module.login.message"));
+    }
+
+    public function onPlayerTransfer(PlayerTransferEvent $event): void {
+        if (!UserSystem::isLogined($event->getPlayer())) {
+            $event->cancel();
+        }
     }
 
 }
